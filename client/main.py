f02977abda599af96b40caaa494fcc9031336ee7
nihiyama <1klyot8qo@gmail.com>
2022-01-31 00:43:08 +0900

    fix only noto coro

--- client/main.py
+++ client/main.py
@@ -18,17 +18,6 @@ def not_coro(interval: int, event: Any):
     print(event)
     time.sleep(interval)
 
-async def execution_task(queue: asyncio.Queue, executor: ThreadPoolExecutor):
-    try:
-        while True:
-            print("aaa")
-            item = await queue.get()
-            print(executor._work_queue.qsize())
-            executor.submit(not_coro, 3, item)
-            
-    except Exception as e:
-        print(e)
-
 
 async def hello_handler(ws: ws_client.WebSocketClientProtocol):
     req_data = {
@@ -42,17 +31,17 @@ async def hello_handler(ws: ws_client.WebSocketClientProtocol):
         raise ws_exceptions.SecurityError(f"token invalid error, {res_data}")
 
 
-async def get_event_handler(ws: ws_client.WebSocketClientProtocol, queue: asyncio.Queue):
+async def get_event_handler(ws: ws_client.WebSocketClientProtocol, executor: ThreadPoolExecutor):
     async for message in ws:
-        queue.put_nowait(message)
-        print("queing", queue.qsize())
+        executor.submit(not_coro, 3, message)
+        print(executor._work_queue.qsize())
 
 
-async def get_event_websocket(queue: asyncio.Queue):
+async def get_event_websocket(executor: ThreadPoolExecutor):
     async for ws in ws_client.connect(settings.WS_SERVER_URL):
         try:
             await hello_handler(ws)
-            await get_event_handler(ws, queue)
+            await get_event_handler(ws, executor)
         except ws_exceptions.ConnectionClosed:
             await asyncio.sleep(3)
             continue
@@ -62,18 +51,10 @@ async def get_event_websocket(queue: asyncio.Queue):
             await ws.close()
 
 async def main():
-    # async with ws_client.connect(settings.WS_SERVER_URL) as ws:
-    #     await hello_handler(ws)
-    #     await get_event_handler(ws)
-    queue: asyncio.Queue = asyncio.Queue()
     with ThreadPoolExecutor(max_workers=5) as executor:
-        exec_task = asyncio.create_task(execution_task(queue, executor))
-        websocket_task = asyncio.create_task(get_event_websocket(queue))
-        tasks = [websocket_task, exec_task]
-        await asyncio.gather(
-            *tasks
-        )
-    
+        await get_event_websocket(executor)
+        print(executor._work_queue.qsize())
+
 
 if __name__ == "__main__":
     asyncio.run(main())
